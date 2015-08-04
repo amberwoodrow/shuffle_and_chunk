7bbfd2de3c43e921240f2aab8f9e2986fe2ac606
amberwoodrow <amberwoodrow@gmail.com>
2015-08-03 20:04:22 -0600

    initial commit

---
+++ js/main.js
@@ -0,0 +1,42 @@
+function shuffle(array){
+  var result = [];
+  var arrayCopy = array.slice(0);
+  arrayLength = array.length;
+  for (var i = 0; i < arrayLength; i++) {
+    var num = Math.floor(Math.random() * arrayCopy.length);
+    result.push(arrayCopy.splice(num, 1)[0]);
+  }
+  return result;
+}
+
+// function chunk (numChunks, array) {
+//   var shuffled = shuffle(array);
+//   var shuffledLength = shuffled.length;
+  // var elementsPerArray = shuffled.length/numChunks;
+//   var chunkedArray = [];
+//   for (i=0; i<=shuffledLength; i++) {
+//     chunkedArray.push(shuffled.splice(i, i + numChunks));
+//   }
+//   return chunkedArray;
+// }
+
+// function chunk (numChunks, array) {
+//   var shuffled = shuffle(array);
+//   var chunkedArray = [];
+//   var elementsPerArray = shuffled.length/numChunks;
+//   for (i=0; i<=(elementsPerArray+1); i++) {
+//     chunkedArray.push(shuffled.slice(i, i + elementsPerArray));
+//   }
+//   return chunkedArray;
+// }
+
+function chunk (numChunks, array) {
+  var i,j,arrCopy;
+  for (i=0,j=array.length; i<j; i+=numChunks) {
+    arrCopy = array.slice(i,i+numChunks);
+    console.log(arrCopy);
+  }
+}
+
+console.log(JSON.stringify(chunk(4, [1,2,3,4,5,6,7,8])));
+console.log(JSON.stringify(chunk(5, [1,2,3,4,5,6,7,8])));
